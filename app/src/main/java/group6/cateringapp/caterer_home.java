307dd2284d16132c971aa3980ef489e846f38e2a
Sanjeet Acharya <sanjeetacharya1@gmail.com>
2018-04-25 15:03:48 -0500

    Caterer HomeScreen, Selected caterer event screen and selected user request screen(screen only) layouts added System home page, Login screen, Registration categories Login works for username as "admin" and password as "password"

---
+++ app/src/main/java/group6/cateringapp/caterer_home.java
@@ -0,0 +1,52 @@
+package group6.cateringapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class caterer_home extends AppCompatActivity {
+    Button b_logout, b_view_cat_event_request, b_view_user_request, b_update_profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caterer_home);
+
+
+        b_view_cat_event_request = (Button) findViewById(R.id.b_view_cat_event_summary);
+        b_view_user_request = (Button) findViewById(R.id.b_view_user_request);
+        b_update_profile = (Button) findViewById(R.id.b_update_profile);
+        b_logout = (Button) findViewById(R.id.b_logout);
+        b_view_cat_event_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSelectedEventScreen();
+
+            }
+        });
+        b_view_user_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUserRequestScreen();
+            }
+        });
+    }
+
+    private void openSelectedEventScreen(){
+            Intent intent = new Intent(this,selected_event_screen.class);
+            startActivity(intent);
+
+
+    };
+    private void openUserRequestScreen(){
+        Intent intent = new Intent(this, user_request_summary.class);
+        startActivity(intent);
+    };
+
+
+
+}
+
+
---
+++ app/src/main/java/group6/cateringapp/selected_event_screen.java
@@ -0,0 +1,13 @@
+package group6.cateringapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selected_event_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selected_event_screen);
+    }
+}
