260294f49e8ff2e8973e65da55dae7a8786197fe
Sanjeet Acharya <sanjeetacharya1@gmail.com>
2018-04-25 15:03:22 -0500

    Caterer HomeScreen, Selected caterer event screen and selected user request screen(screen only) layouts added System home page, Login screen, Registration categories Login works for username as "admin" and password as "password"

--- app/src/main/java/group6/cateringapp/login_screen.java
+++ app/src/main/java/group6/cateringapp/login_screen.java
@@ -1,13 +1,44 @@
 package group6.cateringapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login_screen extends AppCompatActivity {
+    Button b_login;
+    EditText u_name, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        b_login = (Button) findViewById(R.id.b_login);
+        u_name = (EditText) findViewById(R.id.u_name);
+        password = (EditText) findViewById(R.id.password);
+
+        b_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = u_name.getText().toString();
+                String pass = password.getText().toString();
+                if (name.equals("admin") && pass.equals("password")) {
+                    openCatererScreen();
+                }
+
+            }
+
+            ;
+        });
     }
+        private void openCatererScreen(){
+            Intent intent = new Intent(this,caterer_home.class);
+            startActivity(intent);
+
+    };
+
+
 }
