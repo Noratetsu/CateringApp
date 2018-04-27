046c8d18d670ca53425eb8a7e78aadeb0727b80e
Noreatetsu <Noratetsu@gmail.com>
2018-04-27 13:41:51 -0500

    Added Caterer, Staff, User

--- app/src/main/java/g6/Core/Event.java
+++ app/src/main/java/g6/Core/Event.java
@@ -1,9 +1,18 @@
 package g6.Core;
+import java.util.Date;
+import java.time.*;
 
+import g6.Resources.*;
 /**
  * Created by Brandon Keen on 4/26/2018.
  */
 
 public class Event {
-    //TODO stuff into Event
+    Date date;
+    LocalTime time;
+    int duration;
+    Resources resources;
+    int estimatedAttendies;
+    String eventName;
+    EventStatus status;
 }
