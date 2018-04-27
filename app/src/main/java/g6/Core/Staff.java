4f4c57ea336b7067be712e1b30098839ed890010
Noreatetsu <Noratetsu@gmail.com>
2018-04-27 13:33:18 -0500

    Added Caterer, Staff, User

--- app/src/main/java/g6/Core/Staff.java
+++ app/src/main/java/g6/Core/Staff.java
@@ -4,6 +4,5 @@ package g6.Core;
  * Created by Brandon Keen on 4/27/2018.
  */
 
-public enum Role {
-    Staff,User,Caterer;
+public class Staff extends System_User {
 }
--- app/src/main/java/g6/Core/User.java
+++ app/src/main/java/g6/Core/User.java
@@ -4,6 +4,5 @@ package g6.Core;
  * Created by Brandon Keen on 4/27/2018.
  */
 
-public enum Role {
-    Staff,User,Caterer;
+public class User extends System_User {
 }
