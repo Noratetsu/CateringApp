39af8e013d094523002ee65d9fa030fc97a585f1
Sajen Shrestha <Sajen.shrestha@mavs.uta.edu>
2018-04-30 17:47:03 -0500

    Working Request_Event class

--- app/src/main/java/g6/Database/dbmgr.java
+++ app/src/main/java/g6/Database/dbmgr.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import group6.cateringapp.RequestDetails;
 import group6.cateringapp.SignUpDetails;
 /**
  * Created by ssaze on 4/27/2018.
@@ -16,7 +17,37 @@ public class dbmgr extends SQLiteOpenHelper{
     private static final int database_version = 1;
     private static final String database_name = "CateringApp.db";
 
+
+   /////////////////////////////////////////////////////////
+    private static final String table_name = "registration";
+    ////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    private static final String column_id = "id";
+    private static final String column_fname = "FirstName";
+    private static final String column_lname = "LastName";
+    private static final String column_uname = "UserName";
+    private static final String column_usertype = "UserType";
+    private static final String column_email = "Email";
+    private static final String column_phone = "Phone";
+    private static final String column_address = "Address";
+    private static final String column_city = "City";
+    private static final String column_password = "Password";
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    private static final String table_name1 = "UserRequest";
+    ////////////////////////////////////////////////////////
+    private static final String column_RequestId = "RequestID";
+    private static final String column_Date = "Date";
+    private static final String column_PartySize = "PartySeze";
+    private static final String column_Occasion= "Occasion";
+    private static final String column_Duration = "Duration";
+    private static final String column_Time = "Time";
+    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+   // SQLiteDatabase db;
+
        // SQLiteDatabase db;
+
    //String table_create = "CREATE TABLE registration (ID integer primary key not null, FirstName text, LastName text, UserName text, Password text, UserType text, Email text, phone text, Address text, City text);";
 
     private dbmgr (Context context){
@@ -32,8 +63,15 @@ public class dbmgr extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        String table_create = "CREATE TABLE registration (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FirstName TEXT, LastName TEXT, UserName TEXT, Password TEXT, UserType TEXT, Email TEXT, phone TEXT, Address TEXT, City TEXT);";
+        String table_create1 = "CREATE TABLE UserRequest (RequestID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Date TEXT, Time TEXT, Occasion TEXT, Duration TEXT, PartySeze TEXT);";
+        sqLiteDatabase.execSQL(table_create);
+        sqLiteDatabase.execSQL(table_create1);
+
+
         sqLiteDatabase.execSQL(UserTable.CreateSQLTable);
       //  sqLiteDatabase.execSQL(EventTable.CreateSQLTable);
+
 
         /*db.execSQL(table_create);
         this.db = db;*/
@@ -58,6 +96,25 @@ public class dbmgr extends SQLiteOpenHelper{
         db.insert(UserTable.TableInfo.TABLE_NAME, null, values);
         db.close();
     }
+    public void InsertRequestDetails(RequestDetails c){
+        SQLiteDatabase db1 = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        String query = "SELECT * FROM UserRequest;";
+        //Cursor cursor = db.rawQuery(query,null);
+        //  int count = cursor.getCount();
+        //  values.put(column_id, count);
+        values.put(column_Date,c.getDate());
+        values.put(column_Time,c.getTime());
+        values.put(column_Occasion,c.getOccasion());
+        values.put(column_Duration,c.getDuration());
+        values.put(column_PartySize,c.getParty_size());
+
+
+        db1.insert(table_name1, null, values);
+        db1.close();
+    }
+
 
     public String searchPass(String uname){
         SQLiteDatabase db = this.getWritableDatabase();
