0b1964e614ac10d22cfbf0ee58700e9c7371202e
Sanjeet Acharya <sanjeetacharya1@gmail.com>
2018-04-30 01:57:39 -0500

    event_summary_screen GUI done cancel event GUI done. Functionality need to do

--- app/src/main/java/group6/cateringapp/event_summary_screen.java
+++ app/src/main/java/group6/cateringapp/event_summary_screen.java
@@ -1,7 +1,12 @@
 package group6.cateringapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class event_summary_screen extends AppCompatActivity {
 
@@ -10,6 +15,78 @@ public class event_summary_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_summary_screen);
         getSupportActionBar().setTitle("Event Summary");
+        Button b_event1,b_event2, b_event3, b_event4, b_event5, b_event6;
+        TextView t_event1_sum, t_event2_sum,t_event3_sum, t_event4_sum, t_event5_sum,t_event6_sum;
+        b_event1 = (Button)findViewById(R.id.b_event1);
+        b_event2 = (Button)findViewById(R.id.b_event2);
+        b_event3 = (Button)findViewById(R.id.b_event3);
+        b_event4 = (Button)findViewById(R.id.b_event4);
+        b_event5 = (Button)findViewById(R.id.b_event5);
+        b_event6 = (Button)findViewById(R.id.b_event6);
+        t_event1_sum = (TextView) findViewById(R.id.t_event1_sum);
+        t_event2_sum = (TextView) findViewById(R.id.t_event2_sum);
+        t_event3_sum = (TextView) findViewById(R.id.t_event3_sum);
+        t_event4_sum = (TextView) findViewById(R.id.t_event4_sum);
+        t_event5_sum = (TextView) findViewById(R.id.t_event5_sum);
+        t_event6_sum = (TextView) findViewById(R.id.t_event6_sum);
+        b_event1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //function to set TextView with related event goes here
+                //
+                openSelectedEventScreen();
+            };
+        });
+        b_event2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //function to set TextView with related event goes here
+                //
+                openSelectedEventScreen();
+            };
+        });
+        b_event3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //function to set TextView with related event goes here
+                //
+                openSelectedEventScreen();
+            };
+        });
+        b_event4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //function to set TextView with related event goes here
+                //
+                //
+                openSelectedEventScreen();
+            };
+        });
+        b_event5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //function to set TextView with related event goes here
+                //
+                //
+                openSelectedEventScreen();
+            };
+        });
+        b_event6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //function to set TextView with related event goes here
+                //
+                //
+                openSelectedEventScreen();
+            };
+        });
+
+
 
     }
+    private void openSelectedEventScreen(){
+        Intent intent = new Intent(this, selected_event_screen.class);
+        startActivity(intent);
+
+    };
 }
--- app/src/main/java/group6/cateringapp/selected_event_screen.java
+++ app/src/main/java/group6/cateringapp/selected_event_screen.java
@@ -1,16 +1,19 @@
 package group6.cateringapp;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class selected_event_screen extends AppCompatActivity {
 
-    Button b_view_available_staff, b_logout,b_add_resources, b_cancel_event;
+    Button b_view_available_staff, b_logout, b_add_resources, b_cancel_event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,48 +21,96 @@ public class selected_event_screen extends AppCompatActivity {
         setContentView(R.layout.selected_event_screen);
         getSupportActionBar().setTitle("Selected Event Screen");
 
-        b_view_available_staff = (Button)findViewById(R.id.b_view_available_staff);
-        b_logout = (Button)findViewById(R.id.b_logout);
-        b_add_resources = (Button)findViewById(R.id.b_add_resources);
-        b_cancel_event = (Button)findViewById(R.id.b_cancel_event);
-       b_view_available_staff.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               openViewAvailableStaffScreen();
-           };
-       });
-       b_logout.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               openLoginPage();
-           };
-       });
-       b_add_resources.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               openAddResourcePage();
-           }
-       });
-       b_cancel_event.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
+        b_view_available_staff = (Button) findViewById(R.id.b_view_available_staff);
+        b_logout = (Button) findViewById(R.id.b_logout);
+        b_add_resources = (Button) findViewById(R.id.b_add_resources);
+        b_cancel_event = (Button) findViewById(R.id.b_cancel_event);
+        b_view_available_staff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViewAvailableStaffScreen();
+            }
 
-           };
-       });
+            ;
+        });
+        b_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginPage();
+            }
+
+            ;
+        });
+        b_add_resources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddResourcePage();
+            }
+        });
+        b_cancel_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCancelScreen();
+            };
+
+
+        });
 
     }
 
-    private void openViewAvailableStaffScreen(){
+    private void openViewAvailableStaffScreen() {
         Intent intent = new Intent(this, ViewAvailableStaff.class);
         startActivity(intent);
-    };
-    private void openLoginPage(){
+    }
+
+    ;
+
+    private void openLoginPage() {
         Intent intent = new Intent(this, login_screen.class);
         startActivity(intent);
-    };
-    private void openAddResourcePage(){
+    }
+
+    ;
+
+    private void openAddResourcePage() {
         Intent intent = new Intent(this, add_resources.class);
         startActivity(intent);
-    };
+    }
+
+    ;
+
+    public void openCancelScreen(){
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(selected_event_screen.this);
+        a_builder.setMessage("Are you sure you want to cancel this event?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                        //what attributes change after cancelling the event?
+                        //event status change in event summary
+                        //
+                        openEventSummaryScreen();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Cancel Event");
+        alert.show();
+
+    }
+    private void openEventSummaryScreen(){
+        Intent intent = new Intent(this,event_summary_screen.class);
+        startActivity(intent);
+    }
+
 
 }
+
+
+
